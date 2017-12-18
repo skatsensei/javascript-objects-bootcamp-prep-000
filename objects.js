a115b5856226773757820d0dc9d04fc35b084f76
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-12-18 05:19:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,10 @@
 var playlist = {
   oasis: "wonderwall"
 }
-function updatePlaylist(playlist, aristName, songTitle){
+function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
-var playlist = {artistName: "Random"}
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName
   playlist
